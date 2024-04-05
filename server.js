e1b354feb2612badb6a3037ed891ea7f533495e0
Duncan McMurtrie <duncan.mcmurtrie@gmail.com>
2024-04-05 16:58:12 +1100

    Factored out error handling for /add endpoint, but it's too code-heavy and I've determined a better way to do it. Committing to keep my working before erasing it.

--- server.js
+++ server.js
@@ -47,42 +47,49 @@ const divide = (n1, n2) => {
     return n1 / n2;
 }
 
-// Endpoints
-// Adds n1 and n2 and returns their sum
-// Usage: http://localhost:3000/add?n1=1&n2=5
-app.get("/add", (req, res) => {
-    try {
-        // Try to parse the user's provided parameters as numbers
-        const n1 = req.query.n1;
-        const n2 = req.query.n2;
-        const a = parseFloat(n1);
-        const b = parseFloat(n2);
-
-        // If they're not valid numbers, log and throw an error
-        if (isNaN(a)) {
-            logger.error(`Invalid number '${n1}' received for parameter n2.`);
-            throw new Error(`Invalid number '${n1}' received for parameter n1.`);
-        }
-        if (isNaN(b)) {
-            logger.error(`Invalid number '${n2}' received for parameter n2.`);
-            throw new Error(`Invalid number '${n2}' received for parameter n2.`);
-        }
-
-        // If the parameters are OK (i.e. no error was thrown), subtract n2 from n1 and return the result
-        logger.info(`Numbers ${a} and ${b} received for addition.`);
-        const result = add(a, b);
-        res.status(200).json({ statuscode: 200, data: result });
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({statuscode: 500, msg: error.toString() });
+// Helper function to create an ordinal number string from a number,
+// e.g. "1st" from 1, "2nd" from 2, "3rd" from 3, "4th" from 4, etc.
+const numberSuffixer = (num) => {
+    // Numbers between 4 and 20 all end in "th"
+    if (num >= 4 && num <= 20) {
+        console.log('Number >=4 & <= 20')
+        return num + 'th';
     }
-    
-});
+    // Otherwise, check the trailing digit
+    switch (num % 10) {
+        case 1:
+            console.log("Case 1 reached");
+            return num + "st";
+        case 2:
+            return num + "nd";
+        case 3:
+            return num + "rd"
+        default:
+            console.log("Default case reached");
+            return num + "th";
+    }
+};
 
-// Subtracts n2 from n1 and returns this result
-// Usage: http://localhost:3000/subtract?n1=1&n2=5
-app.get("/subtract", (req, res) => {
+// Parameter validator
+// Parses an arbitrary number of strings in an array
+// Returns an array of floats if all pass; if any fail, the first
+// one will be logged, and an error message will be returned as a 
+// string.
+const validateParamsAsFloats = (params) => {
+    let nums = [];
+    let num;
+    for (let i = 0; i < params.length; i++) {
+        num = parseFloat(params[i]);
+        if (isNaN(num)) {
+            const nth = numberSuffixer(i + 1);
+            return `Invalid number '${params[i]}' received for ${nth} parameter`;
+        }
+        nums.push(num);
+    }
+    return nums;
+}
+
+const calculate = (req, res, fun) => {
     try {
         // Try to parse the user's provided parameters as numbers
         const n1 = req.query.n1;
@@ -92,16 +99,16 @@ app.get("/subtract", (req, res) => {
 
         // If they're not valid numbers, log and throw an error
         if (isNaN(a)) {
-            logger.error(`Invalid number '${n1}' received for parameter n2.`);
+            logger.error(`Invalid number '${n1}' received for parameter n2 in function ${fun.name}.`);
             throw new Error(`Invalid number '${n1}' received for parameter n1.`);
         }
         if (isNaN(b)) {
-            logger.error(`Invalid number '${n2}' received for parameter n2.`);
+            logger.error(`Invalid number '${n2}' received for parameter n2 in function ${fun.name}.`);
             throw new Error(`Invalid number '${n2}' received for parameter n2.`);
         }
         
         // If the parameters are OK (i.e. no error was thrown), subtract n2 from n1 and return the result
-        logger.info(`Numbers ${a} and ${b} received for subtraction.`);
+        logger.info(`Numbers ${a} and ${b} received for function ${fun.name}.`);
         const result = subtract(a, b);
         res.status(200).json({ statuscode: 200, data: result });
 
@@ -109,7 +116,40 @@ app.get("/subtract", (req, res) => {
         console.error(error);
         res.status(500).json({statuscode: 500, msg: error.toString() });
     }
+};
+
+// Endpoints
+// Adds n1 and n2 and returns their sum
+// Usage: http://localhost:3000/add?n1=1&n2=5
+// app.get("/add", (req, res) => {
+//     const endpoint = '/add'
+//     try {
+//         // Try to parse the user's provided parameters as numbers
+//         const params = [req.query.n1, req.query.n2];
+//         const validatedParams = validateParamsAsFloats(params);
+//         // Throw an error if validation failed and an error message was returned
+//         if (typeof validatedParams == 'string') {
+//             const errorMessage = validatedParams + ` at '${endpoint}' endpoint.`;
+//             logger.error(errorMessage);
+//             throw new Error(errorMessage);
+//         }
+        
+//         // If the parameters are OK (i.e. no error was thrown), subtract the 2nd parameter from the 1st and return the result
+//         logger.info(`Numbers ${validatedParams[0]} and ${validatedParams[1]} received for addition.`);
+//         const result = add(validatedParams[0], validatedParams[1]);
+//         res.status(200).json({ statuscode: 200, data: result });
+
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({statuscode: 500, msg: error.toString() });
+//     }
     
+// });
+
+// Subtracts n2 from n1 and returns this result
+// Usage: http://localhost:3000/subtract?n1=1&n2=5
+app.get("/subtract", (req, res) => {
+    calculate(req, res, subtract)
 });
 
 // Multiplies n1 by n2 and returns this result
